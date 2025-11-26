75008b5c103102b6589b31a96b90a1fdc48f2ddb
annawei <annawei@benet.com.tw>
2025-11-26 17:31:56 +0800

    1126

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: '/freedom-of-speech', 
-  assetPrefix: '/freedom-of-speech/',
+  basePath: '/freedom-of-speech',
+  assetPrefix: '/freedom-of-speech',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
